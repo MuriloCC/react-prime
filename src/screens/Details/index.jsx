c1508f93a14e19a8e97363cef2cadbb3a401754d
Murilo Castilho <murilo.castilho21@gmail.com>
2021-10-01 23:39:07 -0300

    Details page and modal with web view done

--- src/screens/Details/index.jsx
+++ src/screens/Details/index.jsx
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import Stars from 'react-native-stars';
+import { ScrollView, Modal } from 'react-native';
+
 import { 
     Container, 
-    Title,
     Header,
     HeaderButton, 
-    Banner 
+    Banner,
+    ButtonLink,
+    Title,
+    ContentArea,
+    Rate, 
+    ListGenres,
+    Description
 } from './styles';
+
 import { Feather, Ionicons } from '@expo/vector-icons';
 
 import { api, key } from '../../services/api';
+import { Genres } from '../../components/Genres';
+import { ModalLink } from '../../components/ModalLink';
 
 export function Details(){
     const navigation = useNavigation();
     const route = useRoute();
 
     const [movie, setMovie] = useState({});
+    const [openLink, setOpenLink] = useState(false);
 
     useEffect(() => {
         let isActive = true;
@@ -38,20 +50,23 @@ export function Details(){
             getMovie();
         }
         
-
         return () => isActive = false;
     }, [])
     return(
         <Container>
             <Header>
-                <HeaderButton activeOpacity={0.7} onPress={() => navigation.goBack()}>
+                <HeaderButton 
+                    activeOpacity={0.7} 
+                    onPress={() => navigation.goBack()
+                    }
+                >
                     <Feather 
                         name="arrow-left" 
                         size={28} 
                         color="#fff" 
                     />
                 </HeaderButton>
-                <HeaderButton>
+                <HeaderButton activeOpacity={0.7}>
                     <Ionicons 
                         name="bookmark" 
                         size={28} 
@@ -59,6 +74,56 @@ export function Details(){
                     />
                 </HeaderButton>
             </Header>
+
+            <Banner
+                resizeMethod="resize" 
+                source={{uri: `https://image.tmdb.org/t/p/original/${movie.poster_path}`}}
+            />
+
+            <ButtonLink activeOpacity={0.7} onPress={() => setOpenLink(true)}>
+                <Feather name="link" size={24} color="#fff" />
+            </ButtonLink>
+
+            <Title numberOfLines={2}>{movie.title}</Title>
+
+            <ContentArea>
+                <Stars
+                    default={movie.vote_average}
+                    count={10}
+                    half={true}
+                    starSize={20}
+                    fullStar={<Ionicons name="md-star" size={24} color="#e7a74e" />}
+                    emptyStar={<Ionicons name="md-star-outline" size={24} color="#e7a74e" />}
+                    halfStar={<Ionicons name="md-star-half" size={24} color="#e7a74e" />}
+                    disable={true}
+                />
+                <Rate>{movie.vote_average}/10</Rate>
+            </ContentArea>
+
+            <ListGenres 
+                    data={movie?.genres}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={item => String(item.id)}
+                    renderItem={({ item }) => <Genres data={item} />}
+                />
+            
+            <ScrollView>
+                <Title>Descrição</Title>
+                <Description>{movie?.overview}</Description>
+            </ScrollView>
+
+            <Modal 
+                animationType="slide" 
+                transparent={true} 
+                visible={openLink}
+            >
+                <ModalLink 
+                    link={movie?.homepage}
+                    title={movie.title}
+                    closeModal={() => setOpenLink(false)}
+                />
+            </Modal>
         </Container>
     );
 }
--- src/screens/Details/styles.js
+++ src/screens/Details/styles.js
@@ -1,3 +1,4 @@
+import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -5,7 +6,6 @@ export const Container = styled.View`
     background-color: #191a30;
 
 `;
-export const Title = styled.Text``;
 export const Header = styled.View`
     z-index: 99;
     position: absolute;
@@ -27,7 +27,53 @@ export const HeaderButton = styled.TouchableOpacity`
 `;
 
 export const Banner = styled.Image`
-    
+    width: 100%;
+    height: 350px;
+    border-bottom-left-radius: 50px;
+    border-bottom-right-radius: 50px;
+`;
 
+export const ButtonLink = styled.TouchableOpacity`
+    background-color: #E72F49;
+    width: 63px;
+    height: 63px;
+    border-radius: 35px;
+    position: absolute;
+    top: 300px;
+    right: 15px;
+    justify-content: center;
+    align-items: center;
+    z-index: 99;
+`;
 
+export const Title = styled.Text`
+    color: #fff;
+    font-size: 22px;
+    font-weight: bold;
+    padding: 8px 14px;
+`;
+
+export const ContentArea = styled.View`
+    flex-direction: row;
+    align-items: center;
+    padding: 0 14px;
+    justify-content: space-between;
+`;
+export const Rate = styled.Text`
+    font-size: 18px;
+    font-weight: bold;
+    color: #fff;
+`;
+
+export const ListGenres = styled.FlatList`
+    padding-left: 14px;
+    margin: 8px 0;
+    max-height: 35px;
+    min-height: 35px;
+`;
+
+export const Description = styled.Text`
+    padding: 0 14px 30px 14px;
+    color: #fff;
+    line-height: 20px;
 `;
